3b66c30b7c183f6a0fc88fd3a6f5d4cc1023c0d6
bhadreshpsavani <bhadreshpsavani@gmail.com>
2022-01-13 11:32:50 +0530

    created recipe app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -62,7 +63,17 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView.builder(
             itemCount: Recipe.samples.length,
             itemBuilder: (BuildContext context, int index){
-                return buildRecipeCard(Recipe.samples[index]);
+                return GestureDetector(
+                  onTap: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context){
+                        return RecipeDetail(recipe: Recipe.samples[index]);
+                      })
+                    );
+                  },
+                  child: buildRecipeCard(Recipe.samples[index]),
+                );
             }),
 
       ),
@@ -72,11 +83,30 @@ class _MyHomePageState extends State<MyHomePage> {
 
 Widget buildRecipeCard(Recipe recipe){
   return Card(
-    child: Column(
-      children: <Widget> [
-        Image(image: AssetImage(recipe.imageUrl)),
-        Text(recipe.label),
-    ],
+    elevation: 2.0,
+
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10.0)
     ),
+
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget> [
+          Image(image: AssetImage(recipe.imageUrl)),
+          const SizedBox(
+            height: 14.0,
+          ),
+          Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              )
+          ),
+        ],
+      ),
+    )
   );
 }
